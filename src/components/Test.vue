2447aa4d1980f8400b3c4fb3fe09e852ecddffd1
Daniel Moreira <danielmoreira@sioslife.com>
2018-04-13 15:10:54 +0100

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="Test">
+    <h1>{{title}}</h1>
+    <h2>{{message}}</h2>
+    <input type="text" v-model="title">
+    <div v-if="showUser">
+      <p>User: </p>
+      <p v-text="user.name"></p>
+    </div>
+    <div v-else>Nobody</div>
+
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <label>UserName: <input type="test" v-model="user.name" /></label>
+    
+    <button v-on:click="click()">Click me</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      message: {
+        type: String,
+        default: 'Default Message'
+      }
+    },
+    data () {
+      return {
+        title: 'HI',
+        user: {
+          name: 'John Doe'
+        },
+        showUser: true,
+        items: [
+          {
+            title: 'Page 1'
+          },
+          {
+            title: 'Page 2'
+          },
+          {
+            title: 'Page 3'
+          }
+        ]
+      }
+    },
+    methods: {
+      click: function () {
+        alert('HIIII')
+      }
+    }
+  }
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="Users">
+    <h1>Users</h1>
+    <div v-for="user in users" class="row">
+      <div class="col fill">
+        <span :class="{ active: user.active }">{{user.name}}  {{user.email}}</span>
+      </div>
+      <div class="col s1">
+        <input type="checkbox" v-model="user.active">
+      </div>
+      <div class="col s1">
+        <button v-on:click="deleteUser(user)">X</button>
+      </div>
+      
+    </div>
+
+    <div class="margin15"></div>
+    <div class="margin15"></div>
+
+    <form v-on:submit="addUser">
+    <div class="row">
+      <label class="col m4">
+        Name:        
+      </label>
+      <input class="col fill" type="text" v-model="newUser.name" placeholder="Insert name" />
+    </div>
+    <div class="margin15"></div>
+    <div class="row">
+      <label class="col m4">
+        Email:        
+      </label>
+      <input class="col fill" type="text" v-model="newUser.email" placeholder="Insert email" />
+    </div>
+    <div class="margin15"></div>
+    <div class="row">
+      <button class="col" type="submit">Add</button>
+    </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'users',
+    data: () => {
+      return {
+        users: [
+          {
+            name: 'User 1',
+            email: 'user1@a.com',
+            active: true
+          },
+          {
+            name: 'User 2',
+            email: 'user2@a.com',
+            active: false
+          },
+          {
+            name: 'User 3',
+            email: 'user3@a.com',
+            active: false
+          },
+          {
+            name: 'User 4',
+            email: 'user4@a.com',
+            active: true
+          }
+        ],
+        newUser: {
+          name: '',
+          email: '',
+          active: false
+        }
+      }
+    },
+    methods: {
+      addUser: function (e) {
+        this.users.push(this.newUser)
+        this.newUser = {
+          name: '',
+          email: '',
+          active: false
+        }
+      },
+      deleteUser: function (user) {
+        this.users.splice(this.users.indexOf(user), 1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .active {
+    text-decoration: line-through;
+  }
+
+  .margin15 {
+    margin-top: 15px;
+  }
+</style>
